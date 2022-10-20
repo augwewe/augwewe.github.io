7cf4d517debc279f610e002e18672a31e3b61023
augwewe <1085816416@qq.com>
2022-10-20 19:28:15 +0800

    update

---
+++ src/.vuepress/.temp/internal/pagesComponents.js
@@ -0,0 +1,6 @@
+import { defineAsyncComponent } from 'vue'
+
+export const pagesComponents = {
+  // path: /404.html
+  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"D:/myaugwewe/augwewe.github.io/src/.vuepress/.temp/pages/404.html.vue")),
+}
