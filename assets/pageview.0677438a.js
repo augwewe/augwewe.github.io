02cea3fe4c4605576694c9b4eed8c8674478e124
augwewe <116265998+augwewe@users.noreply.github.com>
2022-10-31 11:16:56 +0000

    Deploying to gh-pages from @ augwewe/augwewe.github.io@0f8ce2682c89c78a19ca3cbfed15e0d63a8f3960 🚀

--- assets/pageview.0677438a.js
+++
@@ -1,1 +0,0 @@
-import{k as e}from"./app.0425bc9c.js";const i=()=>e({serverURL:"https://discuss.augwewe.cn/"});export{i as updatePageview};
---
+++ assets/pageview.c9e09983.js
@@ -0,0 +1,1 @@
+import{i as e}from"./app.e20a12e2.js";const i=()=>e({serverURL:"https://discuss.augwewe.cn/"});export{i as updatePageview};
