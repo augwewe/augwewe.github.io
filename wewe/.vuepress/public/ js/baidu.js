7290e04451ee776ab70930f3f175cf613d1cd0be
augwewe <1085816416@qq.com>
2022-11-03 17:53:48 +0800

    update

--- wewe/.vuepress/public/ js/baidu.js
+++
@@ -1,9 +0,0 @@
-
-    var _hmt = _hmt || [];
-    (function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?3d00ac71fa0d1fd6bde6a85e888a1fcd";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
-
