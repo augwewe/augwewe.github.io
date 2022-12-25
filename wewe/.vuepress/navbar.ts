25f300ca441a533099dcecc7e63a899aa5730dc7
augwewe <1085816416@qq.com>
2022-12-25 16:41:46 +0800

    update

--- wewe/.vuepress/navbar.ts
+++ wewe/.vuepress/navbar.ts
@@ -29,7 +29,7 @@ export const zhNavbar = navbar([
         text:"news",
         children: [
           {
-            text:"plo.",link:""
+            text:"plog.",link:""
           }
         ]
       }
