7ec733de71c011fe236b576f9791c8e7db96a482
augwewe <116265998+augwewe@users.noreply.github.com>
2023-01-08 15:59:48 +0000

    Deploying to gh-pages from @ augwewe/augwewe.github.io@effaf2026ad33a6e8b5a209964038f6bd6d07e0b 🚀

---
+++ assets/MyDay.html.aaf243c6.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as t,c}from"./app.13d5e50c.js";const o={};function r(_,a){return t(),c("div")}const f=e(o,[["render",r],["__file","MyDay.html.vue"]]);export{f as default};
---
+++ assets/pageview.d82ff11a.js
@@ -0,0 +1,1 @@
+import{k as e}from"./app.13d5e50c.js";const i=()=>e({serverURL:"https://discuss.augwewe.cn/"});export{i as updatePageview};
